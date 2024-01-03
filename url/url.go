e9688cdc70bdc81b73bb66c4a0883bba2e371b79
Dima Lipin <dimich3d@ya.ru>
2024-01-03 17:50:11 +0100

    hostname/port + subtests

--- url/url.go
+++ url/url.go
@@ -11,6 +11,22 @@ type Url struct {
 	Path   string
 }
 
+func (u *Url) Port() string { 
+	idx := strings.Index(u.Host, ":")
+	if idx == -1 {
+		return ""
+	}
+	return u.Host[idx+1:]
+}
+
+func (u *Url) Hostname() string {
+	idx := strings.Index(u.Host, ":")
+	if idx == -1 {
+		return u.Host
+	}
+	return u.Host[:idx]
+}
+
 func Parse(url string) (*Url, error) {
 	idx := strings.Index(url, "://")
 	if idx == -1 {
--- url/url_test.go
+++ url/url_test.go
@@ -3,7 +3,7 @@ package url
 import "testing"
 
 func TestParse(t *testing.T) {
-	want := "http"                           // arrange
+	want := "http"                              // arrange
 	u, err := Parse("http://www.google.com/go") // act
 	if err != nil {
 		t.Fatal(err)
@@ -16,5 +16,44 @@ func TestParse(t *testing.T) {
 	}
 	if got, want := u.Path, "/go"; got != want {
 		t.Fatalf("expected path %s, got %s", want, got)
+	}
+}
+
+func TestPort(t *testing.T) {
+	data := map[string]struct {
+		host string
+		want string
+	}{
+		"no port":   {"www.google.com", ""},
+		"with port": {"www.google.com:80", "80"},
+		"localhost": {"localhost:8080", "8080"},
+		"443":       {"www.google.com:443", "443"},
+	}
+	for name, test := range data {
+		t.Run(name, func(t *testing.T) {
+			u := &Url{Host: test.host}
+			if got := u.Port(); got != test.want {
+				t.Errorf("expected  %s, got %s", test.want, got)
+			}
+		})
+	}
+}
+
+func TestHostname(t *testing.T) {
+	data := map[string]struct {
+		host string
+		want string
+	}{
+		"no port":   {"www.google.com", "www.google.com"},
+		"with port": {"www.google.com:80", "www.google.com"},
+		"localhost": {"localhost:8080", "localhost"},
+	}
+	for name, test := range data {
+		t.Run(name, func(t *testing.T) {
+			u := &Url{Host: test.host}
+			if got := u.Hostname(); got != test.want {
+				t.Fatalf("expected  %s, got %s", test.want, got)
+			}
+		})
 	}
 }
